db328c04a2980b40ca3658276dccf4e970d946e7
Mountain1210 <mountain_wang@126.com>
2019-10-09 13:39:49 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2019-10-09 10:41:25
+ * @version $Id$
+ */
+
+var a:string = "Hello typescript,终于见面了"
+console.log(a)
+let element=document.createElement('div')
+element.innerHTML=a
+document.body.appendChild(element)
