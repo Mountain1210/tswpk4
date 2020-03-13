7bbce8bf44849fd44f8d98bc0c4b2b31393b0d7e
Mountain1210 <mountain_wang@126.com>
2020-03-13 16:26:00 +0800

    属性装饰器

--- src/index.ts
+++ src/index.ts
@@ -277,6 +277,36 @@ function parse(type) {
 }
 let modal = new Modal();
 console.log(modal.addOne('10')); // 11
+
+
+
+ class Point {
+    private _x: number;
+    private _y: number;
+    constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
+    }
+
+    @configurable(false)
+    get x() { return this._x; }
+
+    @configurable(false)
+    get y() { return this._y; }
+}
+
+function configurable(value: boolean) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+        console.log(target);
+        console.log(propertyKey)
+        console.log(descriptor)
+        descriptor.configurable = value;
+    };
+}
+
+
+
 console.log('装饰器部分结束');
 
 
